dce82deb591aaaac68f96725ca0d87e8aba74d1b
shimosawa <shimosawa@questar.ac>
2021-02-27 22:18:38 +0900

    show api sample

--- app/server.go
+++ app/server.go
@@ -11,5 +11,14 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+	// show api --> return json
+	// url --> '/users/:id"
+	e.GET("users/:id", func(c echo.Context) error {
+		jsonMap := map[string]string{
+			"name": "example",
+			"email": "example@example.com",
+		}
+		return c.JSON(http.StatusOK, jsonMap)
+	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
