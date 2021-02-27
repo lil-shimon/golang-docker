0feff20fe6c51932c6131c7f4b345dca753383a4
shimosawa <shimosawa@questar.ac>
2021-02-27 22:29:33 +0900

    edit server.go

--- app/server.go
+++ app/server.go
@@ -3,22 +3,25 @@ package main
 import (
 	"net/http"
 
-	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo"
 )
+
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	// show api --> return json
-	// url --> '/users/:id"
-	e.GET("users/:id", func(c echo.Context) error {
-		jsonMap := map[string]string{
-			"name": "example",
-			"email": "example@example.com",
-		}
-		return c.JSON(http.StatusOK, jsonMap)
-	})
+	e.GET("/user", show)
+
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func show(c echo.Context) error {
+	u := new(User)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, u)
 }
