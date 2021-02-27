15b15eef3d2854f2160e100c1f683b7add306e09
shimosawa <shimosawa@questar.ac>
2021-02-27 22:42:30 +0900

    postしたときに成功したらメッセージを送るsend message apiを作成

--- app/server.go
+++ app/server.go
@@ -11,10 +11,26 @@ type User struct {
 	Email string `json:"email"`
 }
 
+//受け取り用の構造体
+type Message struct {
+	Name string `json:"name"`
+	Email string `json:"email"`
+	Message string `json:"message"`
+}
+
+//レスポンス用の構造体
+type Response struct {
+	Name string `json:"name"`
+	Email string `json:"email"`
+	Message string `json:"message"`
+	Status string `json:"status"`
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/user", show)
 	e.POST("/user", post)
+	e.POST("/send", sendMessage)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
@@ -35,4 +51,17 @@ func post(e echo.Context) error {
 		return err
 	}
 	return c.JSON(http.StatusOK, u)
+}
+
+func sendMessage(e echo.Context) error {
+	m := new(Message)
+	if error := c.Bind(m); error != nil {
+		return error
+	}
+	r := new(Response)
+	r.Name = m.Name
+	r.Email= m.Email
+	r.Message = m.Message
+	r.Status = "success"
+	return c.JSON(http.StatusOK, r)
 }
