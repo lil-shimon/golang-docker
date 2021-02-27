b580276a7bceaaaa9cb831d52f35ba919256c7dc
shimosawa <shimosawa@questar.ac>
2021-02-27 22:35:00 +0900

    post api

--- app/server.go
+++ app/server.go
@@ -14,11 +14,22 @@ type User struct {
 func main() {
 	e := echo.New()
 	e.GET("/user", show)
+	e.POST("/user", post)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
+//show api
 func show(c echo.Context) error {
+	u := new(User)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, u)
+}
+
+//post api
+func post(e echo.Context) error {
 	u := new(User)
 	if err := c.Bind(u); err != nil {
 		return err
